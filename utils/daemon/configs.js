604ef921facf5c617e0513b62b6febbb759376e4
John Lam <johnlamyc@gmail.com>
2018-08-31 16:14:26 -0700

    Add dynamic daemon config generation and uuidlist read

--- utils/daemon/configs.js
+++ utils/daemon/configs.js
@@ -1,4 +1,7 @@
 const fs = require('fs');
+const fsPromises = require('fs').promises;
+
+const uuids = require('./uuids');
 
 module.exports = {
     editFile: ({filename, changes, remove, deleteKey}) => {
@@ -21,8 +24,49 @@ module.exports = {
         fs.writeFileSync(`./daemon-build/output/${filename}`, JSON.stringify(fileContent), 'utf8');
 
         return fileContent
+    },
+
+    generateConfigs: async (numOfConfigs) => {
+
+        const template = readTemplate('./configs/template.json');
+
+        let configsWithIndex = [...Array(numOfConfigs).keys()].map(() => {
+            let currentIndex = configCounter();
+
+            if (currentIndex > uuids.length - 1) {
+                throw new Error('Ran out of UUIDs to generate configs with')
+            }
+
+            return {
+                content: new Config(template,
+                    {
+                        listener_port: template.listener_port + currentIndex,
+                        http_port: template.http_port + currentIndex,
+                        uuid: uuids[currentIndex]
+                    }),
+                index: currentIndex
+            }
+        });
+
+        await Promise.all(configsWithIndex.map((obj) =>
+            fsPromises.writeFile(`./daemon-build/output/bluzelle${obj.index}.json`, JSON.stringify(obj.content))));
     }
 };
+
+const configCounter = (() => {
+    let counter = -1;
+    return () => counter += 1;
+})();
+
+function Config(keys, edits) {
+    Object.entries(keys).forEach((key) => this[key[0]] = key[1]);
+
+    if (edits) {
+        Object.entries(edits).forEach(key => this[key[0]] = key[1])
+    }
+};
+
+const readTemplate = path => JSON.parse(fs.readFileSync(path).toString());
 
 const setValues = (fileContent, changes) => {
     for (let key in changes) {
