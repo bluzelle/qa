9114665f58e489d45e3bff5edf213a1cf6cae3a3
John Lam <john@bluzelle.com>
2019-02-15 17:14:26 -0800

    Add tests for empty/loaded db

--- test/dynamic-peers.test.js
+++ test/dynamic-peers.test.js
@@ -1,5 +1,6 @@
+const assert = require('assert');
 const common = require('./common');
-const {startSwarm, initializeClient, teardown, spawnDaemon} = require('../utils/daemon/setup');
+const {startSwarm, initializeClient, teardown, spawnDaemon, createKeys} = require('../utils/daemon/setup');
 const {createDirectories, generateConfigs, generatePeersList} = require('../utils/daemon/configs');
 const fsPromises = require('fs').promises;
 const PollUntil = require('poll-until-promise');
@@ -13,99 +14,130 @@ const numOfNodes = harnessConfigs.numOfNodes;
 
 describe('dynamic peering', () => {
 
-    [{name: 'new peer bootstrapped with full peers list', numOfNodesToBootstrap: numOfNodes}, {name: 'new peer bootstrapped with one peer', numOfNodesToBootstrap: 1}].forEach((test) => {
+    [{
+        name: 'add peer with no state',
+        numOfKeys: 0
+    }, {
+        name: 'add peer with loaded db',
+        numOfKeys: 50
+    }].forEach((ctx) => {
 
-        context.only(test.name, function() {
+        context(ctx.name, function() {
+            [{
+                name: 'new peer bootstrapped with full peers list',
+                numOfNodesToBootstrap: numOfNodes
+            }, {
+                name: 'new peer bootstrapped with one peer',
+                numOfNodesToBootstrap: 1
+            }].forEach((test) => {
 
-            before('stand up swarm and client', async function () {
-                this.timeout(30000);
-                await setup.call(this, numOfNodes, test.numOfNodesToBootstrap);
-            });
+                context(test.name, function() {
 
-            after('remove configs and peerslist and clear harness state', function () {
-                teardown.call(this.currentTest, process.env.DEBUG_FAILS);
-            });
+                    before('stand up swarm and client', async function () {
+                        this.timeout(30000);
 
-            it('should successfully join swarm', async function () {
+                        [this.swarm, peersList] = await startSwarm({numOfNodes});
 
-                await new Promise(res => {
-                    this.swarm[`daemon${this.newPeerIdx}`].stream.stdout.on('data', (data) => {
+                        this.api = await initializeClient({swarm: this.swarm, setupDB: true, log: true});
 
-                        if (data.toString().includes('Successfully joined the swarm')) {
-                            res();
+                        clientsObj.api = this.api;
+
+                        if (ctx.numOfKeys > 0) {
+                            await createKeys(clientsObj, ctx.numOfKeys)
                         }
+
+                        const {daemonDirPath, data} = await generateNewPeer(numOfNodes);
+
+                        this.newPeerIdx = data.index;
+
+                        addPeerToSwarmObj.call(this, data);
+
+                        const culledPeersList = peersList.slice(0, test.numOfNodesToBootstrap);
+
+                        await writePeersList(culledPeersList, test.numOfNodesToBootstrap, daemonDirPath);
+
+                        await spawnDaemon(this.swarm, this.newPeerIdx);
                     });
-                });
-            });
 
-            it('should increment swarm view number by 1', async function () {
-                this.timeout(40000);
+                    after('remove configs and peerslist and clear harness state', function () {
+                        teardown.call(this.currentTest, process.env.DEBUG_FAILS);
+                    });
 
-                const pollView = new PollUntil();
+                    it('should successfully join swarm', async function () {
 
-                await new Promise((resolve, reject) => {
-                    pollView
-                        .stopAfter(40000)
-                        .tryEvery(2000)
-                        .execute(() => new Promise((res, rej) => {
+                        await new Promise(res => {
+                            this.swarm[`daemon${this.newPeerIdx}`].stream.stdout.on('data', (data) => {
 
-                            this.api.status().then(val => {
-
-                                const something = JSON.parse(val.moduleStatusJson).module[0].status;
-
-                                if (something.view === 2) {
-                                    return res(true)
-                                } else {
-                                    return rej()
+                                if (data.toString().includes('Successfully joined the swarm')) {
+                                    res();
                                 }
                             });
-                        }))
-                        .then(() => resolve())
-                        .catch(err => console.log(err) || reject(''))
+                        });
+                    });
+
+                    it('should increment swarm view number by 1', async function () {
+                        this.timeout(40000);
+
+                        const pollView = new PollUntil();
+
+                        await new Promise((resolve, reject) => {
+                            pollView
+                                .stopAfter(40000)
+                                .tryEvery(2000)
+                                .execute(() => new Promise((res, rej) => {
+
+                                    this.api.status().then(val => {
+
+                                        const response = JSON.parse(val.moduleStatusJson).module[0].status;
+
+                                        if (response.view === 2) {
+                                            return res(true)
+                                        } else {
+                                            return rej()
+                                        }
+                                    });
+                                }))
+                                .then(() => resolve())
+                                .catch(err => console.log(err) || reject(''))
+
+                        });
+                    });
+
+                    it('should be included in status response', async function () {
+
+                        const res = await this.api.status();
+
+                        const parsedStatusJson = JSON.parse(res.moduleStatusJson).module[0].status;
+
+                        parsedStatusJson.peer_index.should.contain.an.item.with.property('uuid', this.swarm[`daemon${this.newPeerIdx}`].uuid)
+                    });
+
+                    if (ctx.numOfKeys > 0) {
+
+                        it('should be able to fetch full keys list', async function () {
+                            assert((await clientsObj.api.keys()).length === ctx.numOfKeys);
+                        });
+
+                        it('should be able to read last key before pre-primary failure', async function () {
+                            assert((await clientsObj.api.read(`batch${ctx.numOfKeys - 1}`)) === 'value')
+                        })
+                    }
+
+                    common.crudFunctionalityTests(clientsObj);
+
+                    common.miscFunctionalityTests(clientsObj);
 
                 });
+
             });
-
-            it('should be included in status response', async function () {
-
-                const res = await this.api.status();
-
-                const parsedStatusJson = JSON.parse(res.moduleStatusJson).module[0].status;
-
-                parsedStatusJson.peer_index.should.contain.an.item.with.property('uuid', this.swarm[`daemon${this.newPeerIdx}`].uuid)
-            });
-
-            common.crudFunctionalityTests(clientsObj);
-
-            common.crudFunctionalityTests(clientsObj);
-
         });
 
-    });
+    })
 
     // todo: add test for mixed configs, signing disabled
     //  add test for old clients
 
 });
-
-async function setup(numOfNodes, numOfNodesToIncludeInPeersList) {
-    [this.swarm, peersList] = await startSwarm({numOfNodes});
-    this.api = await initializeClient({swarm: this.swarm, setupDB: true});
-
-    clientsObj.api = this.api;
-
-    const {daemonDirPath, data} = await generateNewPeer(numOfNodes);
-
-    this.newPeerIdx = data.index;
-
-    addPeerToSwarmObj.call(this, data);
-
-    const culledPeersList = peersList.slice(0, numOfNodesToIncludeInPeersList);
-
-    await writePeersList(culledPeersList, numOfNodesToIncludeInPeersList, daemonDirPath);
-
-    await spawnDaemon(this.swarm, this.newPeerIdx);
-}
 
 async function generateNewPeer(numOfExistingPeers) {
     const daemonDirPath = await createDirectories(1, numOfExistingPeers);
