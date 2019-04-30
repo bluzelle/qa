4b36b02ac28790856e4090b2a27357f85d8be523
John Lam <john@bluzelle.com>
2019-04-29 20:26:19 -0700

    Refactor logs tests to use new api

--- tests/integration/logs.test.js
+++ tests/integration/logs.test.js
@@ -1,153 +1,142 @@
-const {exec, execSync, spawn} = require('child_process');
-const {includes} = require('lodash');
-const waitUntil = require("async-wait-until");
-
-const {despawnSwarm, clearDaemonState, clearDaemonStateAndConfigs} = require('../../utils/daemon/setup');
-const {editFile, generateSwarmJsonsAndSetState} = require('../../utils/daemon/configs');
-const {readDir} = require('../../utils/daemon/logs');
+const {spawn} = require('child_process');
+const {initializeClient, createKeys} = require('../../utils/daemon/setup');
+const {readDaemonFile, writeDaemonFile, readDaemonDirectory, getDaemonOutputDir, readDaemonFileSize} = require('../../utils/FileService');
+const {generateSwarm} = require('../../utils/daemonManager');
 
 
-describe('logging', () => {
+describe('logging', function () {
 
-    beforeEach('generate configs and set harness state', async () =>
-        await generateSwarmJsonsAndSetState(1));
+    const DAEMON_OBJ = {
+        listener_port: 50000,
+        directory_name: 'daemon-50000',
+        config_name: 'bluzelle-50000.json'
+    };
 
-    after('remove configs and peerslist and clear harness state', () => {
-        clearDaemonStateAndConfigs();
+    beforeEach('generate configs and set harness state', async function () {
+        this.swarm = generateSwarm({numberOfDaemons: 1});
     });
 
-    context('with log_to_stdout: true', () => {
+    afterEach('remove configs and peerslist and clear harness state', async function () {
+        await this.swarm.stop();
+        this.swarm.removeSwarmState();
+    });
 
-        let node;
-        let output = '';
+    context('with log_to_stdout: true', function () {
 
-        beforeEach('clear daemon state', clearDaemonState);
+        beforeEach('edit config file', function () {
+            const configFile = readDaemonFile(DAEMON_OBJ, DAEMON_OBJ.config_name).run();
 
-        beforeEach('edit config file', () => {
-            editFile({
-                filepath: 'daemon0/bluzelle0.json',
-                changes: {
-                    log_to_stdout: true,
-                    logfile_rotation_size: '2K',
-                    logfile_max_size: '10K',
-                    logfile_dir: 'newlogsdir/'
-                }
+            configFile.log_to_stdout = true;
+
+            writeDaemonFile(DAEMON_OBJ, DAEMON_OBJ.config_name, configFile).run();
+        });
+
+        beforeEach('start daemon', async function () {
+            await this.swarm.start();
+        });
+
+        it('should output to stdout', async function () {
+            await new Promise(res => {
+                this.swarm.getDaemons()[0].getProcess().stdout.on('data', () => {
+                    res()
+                })
             });
-        });
-
-        beforeEach('start daemon', () => {
-            node = spawn(`./swarm`,  ['-c', `bluzelle0.json`], {cwd: `./daemon-build/output/daemon0`});
-
-            node.stdout.on('data', data => {
-                output += data.toString();
-            });
-        });
-
-        afterEach('kill daemons', despawnSwarm);
-
-        it('should create a log', async () => {
-
-            await waitUntil(() => includes(readDir('output/daemon0'), 'newlogsdir'));
-
-            await waitUntil(() => includes(readDir('output/daemon0/newlogsdir')[0], '.log'));
-
-            const logs = readDir('output/daemon0/newlogsdir/');
-
-            expect(logs[0]).to.have.string('.log')
-        });
-
-        it('should output to stdout', async () => {
-
-            await waitUntil(() => output.includes('Running node with ID'))
-        });
-
-        it('should be able to change logs dir path and name', async () => {
-
-            await waitUntil(() => readDir('output/daemon0').includes('newlogsdir'));
-
-            expect(readDir('output/daemon0').includes('newlogsdir')).to.be.true;
 
         });
     });
 
-    context('with log_to_stdout: false', () => {
+    context('with log_to_stdout: false', function () {
 
-        let node, chunk;
+        beforeEach('edit config file', function () {
+            const configFile = readDaemonFile(DAEMON_OBJ, DAEMON_OBJ.config_name).run();
 
-        beforeEach('edit config file', () => {
-            editFile({
-                filepath: 'daemon0/bluzelle0.json',
-                changes: {
-                    log_to_stdout: false,
-                    logfile_rotation_size: '2K',
-                    logfile_max_size: '10K',
-                    logfile_dir: 'newlogsdir/'
-                }
+            configFile.log_to_stdout = false;
+
+            writeDaemonFile(DAEMON_OBJ, DAEMON_OBJ.config_name, configFile).run();
+        });
+
+        beforeEach('start daemon', async function () {
+            // manually spawn daemon because swarm.start() depends on stdout
+            await new Promise(res => {
+                this.daemon = spawn('./swarm', ['-c', DAEMON_OBJ.config_name], {cwd: getDaemonOutputDir(DAEMON_OBJ)});
+                setTimeout(res, 2000);
             });
         });
 
-        beforeEach('start daemon', () => {
-            node = spawn(`./swarm`,  ['-c', `bluzelle0.json`], {cwd: `./daemon-build/output/daemon0`});
+        afterEach('stop daemon', function () {
+            this.daemon.kill()
+        });
 
-            chunk = 0;
+        it('should not output to stdout', async function () {
 
-            node.stdout.on('data', data => {
-                chunk += 1;
+            await new Promise((res, rej) => {
+                this.daemon.stdout.on('data', () => {
+                    rej(new Error('Unexpected daemon stdout output'));
+                });
+
+                setTimeout(res, 2000);
             });
-        });
 
-        afterEach('kill daemons', despawnSwarm);
-
-        it('should create a log', async () => {
-
-            await waitUntil(() => includes(readDir('output/daemon0/newlogsdir')[0], '.log'));
-
-            const logs = readDir('output/daemon0/newlogsdir/');
-
-            expect(logs[0]).to.have.string('.log')
-        });
-
-        it('should not output to stdout', (done) => {
-
-            setTimeout(() => {
-                if (chunk === 0) {
-                    done()
-                } else {
-                    throw new Error(`Received: ${chunk} chunks of data, expected: 0 `)
-                }
-            }, 2000)
         });
     });
 
-    context('log sizes', () => {
 
-        it('should not have files over set limit', (done) => {
-            exec("cd ./daemon-build/output/daemon0/newlogsdir; ls -l | awk '{print $5}' ", (error, stdout, stderr) => {
+    context('logs path and name is customizable', function () {
 
-                const sizes = stdout.trim().split('\n').map(Number);
+        beforeEach('edit config file', function () {
+            const configFile = readDaemonFile(DAEMON_OBJ, DAEMON_OBJ.config_name).run();
 
-                // maximum are approximates, boost allows large writes to complete
-                if (sizes.every(size => size < 2100)) {
-                    done()
-                } else {
-                    throw new Error(`Expected log sizes to be < 2100. Sizes: ${sizes}`)
-                }
-            });
+            configFile.logfile_dir = 'newlogsdir/';
+
+            writeDaemonFile(DAEMON_OBJ, DAEMON_OBJ.config_name, configFile).run();
         });
 
-        it('dir should not total over set limit', done => {
-            exec("cd ./daemon-build/output/daemon0/newlogsdir; ls -l | awk '{print $5}' ", (error, stdout, stderr) => {
+        beforeEach('start daemon', async function () {
+            await this.swarm.start();
+        });
 
-                const sizes = stdout.trim().split('\n').map(Number);
+        it('should create custom logs dir', function () {
+            readDaemonDirectory(DAEMON_OBJ.directory_name).run().should.include('newlogsdir');
+        });
 
-                // maximum are approximates, boost allows large writes to complete
-                const total = sizes.reduce((total, num) => total + num);
-                if (total < 11000) {
-                    done()
-                } else {
-                    throw new Error(`Expected directory total to be < 11000. Total: ${total}`)
-                }
-            });
+        it('should create log in custom logs dir', function () {
+            readDaemonDirectory(`${DAEMON_OBJ.directory_name}/newlogsdir`).run()[0].match(/\.log/).should.not.be.null;
+        });
+    });
+
+
+    context('log sizes', function () {
+
+        beforeEach('edit config file', function () {
+            const configFile = readDaemonFile(DAEMON_OBJ, DAEMON_OBJ.config_name).run();
+
+            configFile.logfile_rotation_size = '2K';
+            configFile.logfile_max_size = '10K';
+
+            writeDaemonFile(DAEMON_OBJ, DAEMON_OBJ.config_name, configFile).run();
+        });
+
+        beforeEach('start daemon, initialize client, and create keys', async function () {
+            await this.swarm.start();
+            this.api = await initializeClient({setupDB: true, log: false});
+            await createKeys({api: this.api}, 50);
+        });
+
+        it('should not have files over set limit', function () {
+            const logFiles = readDaemonDirectory(`${DAEMON_OBJ.directory_name}/logs`).run();
+            const sizes = logFiles.map(logFile => readDaemonFileSize(DAEMON_OBJ, '/logs', logFile).run()['size']);
+
+            // maximum are approximates, boost allows large writes to complete
+            sizes.should.all.be.below(2100);
+        });
+
+        it('dir should not total over set limit', function () {
+            const logFiles = readDaemonDirectory(`${DAEMON_OBJ.directory_name}/logs`).run();
+            const sizes = logFiles.map(logFile => readDaemonFileSize(DAEMON_OBJ, '/logs', logFile).run()['size']);
+            const totalDirectorySize = sizes.reduce((sum, val) => sum += val, 0);
+
+            // maximum is approximate, boost allows large writes to complete
+            totalDirectorySize.should.be.below(12000);
         })
     });
 });
--- utils/FileService.js
+++ utils/FileService.js
@@ -1,4 +1,4 @@
-const {outputJsonSync, readJsonSync, removeSync, copySync} = require('fs-extra');
+const {outputJsonSync, readJsonSync, removeSync, copySync, readdirSync, statSync} = require('fs-extra');
 const {resolve: resolvePath} = require('path');
 const {IO} = require('monet');
 const {curry} = require('lodash/fp');
@@ -11,6 +11,12 @@ exports.writeDaemonFile = curry((daemonConfig, filename, data) =>
 
 exports.readDaemonFile = (daemonConfig, filename) =>
     IO(() => readJsonSync(`${getDaemonOutputDir(daemonConfig)}/${filename}`));
+
+exports.readDaemonFileSize = (daemonConfig, dirName, filename) =>
+    IO(() => statSync(`${getDaemonOutputDir(daemonConfig)}${dirName}/${filename}`));
+
+exports.readDaemonDirectory = (filename) =>
+    IO(() => readdirSync(resolvePath(`${__dirname}/output/${filename}`)));
 
 exports.removeDaemonDirectory = () =>
     IO(() => removeSync(resolvePath(__dirname, './output')));
