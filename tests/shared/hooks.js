afe44eed91d0d6261b1d35e93c240da556263c61
John Lam <john@bluzelle.com>
2019-06-03 19:56:26 -0700

    Update shared/hooks.js

--- tests/shared/hooks.js
+++ tests/shared/hooks.js
@@ -1,5 +1,4 @@
-const {generateSwarm, swarmManager} = require('../../src/daemonManager');
-const swarmRegistry = require('../../src/swarmRegistryAdapter');
+const {swarmManager} = require('../../src/swarmManager');
 
 exports.remoteSwarmHook = function ({createDB = true} = {}) {
     before('initialize client and setup db', async function () {
@@ -26,21 +25,25 @@ exports.remoteSwarmHook = function ({createDB = true} = {}) {
 
 exports.localSwarmHooks = function ({beforeHook = before, afterHook = after, createDB = true, numOfNodes = harnessConfigs.numOfNodes, preserveSwarmState = false} = {}) {
     beforeHook('start swarm and client, create db', async function () {
-        this.timeout(10000);
+        this.timeout(harnessConfigs.defaultBeforeHookTimeout);
 
         this.swarmManager = await swarmManager();
         this.swarm = await this.swarmManager.generateSwarm({numberOfDaemons: numOfNodes});
 
         await this.swarmManager.startAll();
 
-        this.api = await bluzelle({
+        this.apis = await bluzelle({
+            uuid: Math.random().toString(),
             ethereum_rpc: harnessConfigs.ethereumRpc,
-            contract_address: harnessConfigs.esrContractAddress,
+            contract_address: this.swarmManager.getEsrContractAddress(),
             private_pem: harnessConfigs.masterPrivateKey,
             public_pem: harnessConfigs.masterPublicKey,
             _connect_to_all: true,
             log: false,
+            logDetailed: false
         });
+
+        this.api = this.apis[0];
 
         if (createDB) {
             await this.api.createDB();
@@ -49,6 +52,8 @@ exports.localSwarmHooks = function ({beforeHook = before, afterHook = after, cre
 
     afterHook('remove configs and peerslist and clear harness state', async function () {
         await this.swarmManager.stopAll();
-        this.swarmManager.removeSwarmState();
+        if (!preserveSwarmState) {
+            this.swarmManager.removeSwarmState();
+        }
     });
 };
