eff9df43266e825df1eae2af88ff492e6b7f0eef
John Lam <john@bluzelle.com>
2019-07-02 02:53:07 -0700

    Add swarm_info_esrl_url startup tests

--- tests/integration/startup.test.js
+++ tests/integration/startup.test.js
@@ -64,6 +64,43 @@ describe('daemon startup', function () {
             });
         });
     });
+
+    context('esr location', function () {
+
+        context('default', function () {
+
+            beforeEach('edit config file', function () {
+                editConfigFile(DAEMON_OBJ, null, ['swarm_info_esr_url']);
+            });
+
+            it('should default to infura', function (done) {
+                if (launchDaemon(['-c', DAEMON_OBJ.config_name]).stdout.includes('Connecting to: ropsten.infura.io...')) {
+                    done();
+                }
+            });
+
+        });
+
+        context('should be configurable with swarm_info_esr_url', function () {
+
+            beforeEach('edit config file', function () {
+                editConfigFile(DAEMON_OBJ, [['swarm_info_esr_url', 'http://127.0.0.1:8545']]);
+            });
+
+            it('should connect to passed address', function (done) {
+                if (launchDaemon(['-c', DAEMON_OBJ.config_name]).stdout.includes('Connecting to: 127.0.0.1...')) {
+                    done();
+                }
+            });
+
+            it('should not attempt to connect to infura', function (done) {
+                setTimeout(done, 3000);
+                if (launchDaemon(['-c', DAEMON_OBJ.config_name]).stdout.includes('Connecting to: ropsten.infura.io...')) {
+                    throw new Error('Attempted to connect to infura');
+                }
+            });
+        });
+    });
 });
 
 function launchDaemon(swarmExecutableArguments) {
