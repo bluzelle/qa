39f4c63899a08ffd79065724af0f855ab3b08def
John Lam <john@bluzelle.com>
2019-07-03 19:01:05 -0700

    Allow env override on uuid, master pub and priv key

--- tests/test.configurations.js
+++ tests/test.configurations.js
@@ -17,15 +17,14 @@ const harnessConfigs = global.harnessConfigs = {
     numOfNodes: process.env.NUM_OF_NODES ? parseInt(process.env.NUM_OF_NODES) : 4,
     testRemoteSwarm: process.env.TEST_REMOTE_SWARM ? process.env.TEST_REMOTE_SWARM : false,
 
-    defaultUuid: '96dd6297-d8ed-4bd1-a91e-ed2958cda3c7',
-    masterPrivateKey: 'MHQCAQEEIEOd7E9zSxgJjtpGzK/gHl0vVSOZ2iF3TY50InD67BnHoAcGBSuBBAAKoUQDQgAEE/Yeq9sYdyeou+TnNEJjMnuntrzqcFIfIHd49LW461d55TY4hVX66ZXXGvAWRqMVMeELtYuKGYU44bPaxTb1ig==',
-    masterPublicKey: 'MFYwEAYHKoZIzj0CAQYFK4EEAAoDQgAEE/Yeq9sYdyeou+TnNEJjMnuntrzqcFIfIHd49LW461d55TY4hVX66ZXXGvAWRqMVMeELtYuKGYU44bPaxTb1ig==',
+    defaultUuid: process.env.DEFAULT_UUID ? process.env.DEFAULT_UUID : '96dd6297-d8ed-4bd1-a91e-ed2958cda3c7',
+    masterPrivateKey: process.env.MASTER_PRIVATE_KEY ? process.env.MASTER_PRIVATE_KEY : 'MHQCAQEEIEOd7E9zSxgJjtpGzK/gHl0vVSOZ2iF3TY50InD67BnHoAcGBSuBBAAKoUQDQgAEE/Yeq9sYdyeou+TnNEJjMnuntrzqcFIfIHd49LW461d55TY4hVX66ZXXGvAWRqMVMeELtYuKGYU44bPaxTb1ig==',
+    masterPublicKey: process.env.MASTER_PUBLIC_KEY ? process.env.MASTER_PUBLIC_KEY : 'MFYwEAYHKoZIzj0CAQYFK4EEAAoDQgAEE/Yeq9sYdyeou+TnNEJjMnuntrzqcFIfIHd49LW461d55TY4hVX66ZXXGvAWRqMVMeELtYuKGYU44bPaxTb1ig==',
 
     ethereumRpc: process.env.ETHEREUM_RPC ? process.env.ETHEREUM_RPC : 'http://127.0.0.1:8545',
     esrContractAddress: process.env.ESR_CONTRACT_ADDRESS ? process.env.ESR_CONTRACT_ADDRESS : '0x7FDbE549D8b47b8285ff106E060Eb9C43Fd879e5',
 
     initialDaemonListenerPort: 50000,
-    initialDaemonHttpPort: 8080,
 
     keyCreationTimeoutMultiplier: process.env.KEY_CREATION_TIMEOUT_MULTIPLIER ? process.env.KEY_CREATION_TIMEOUT_MULTIPLIER : 5000, // time allotted per key created in before hooks of dynamically generated tests
     daemonStartTimeout: 5000,
