f05700c02f15bb133e7358edd4544ef617c78d41
John Lam <john@bluzelle.com>
2019-07-12 09:14:08 -0700

    Update status request tests, update pbftModule and crudModule schemas

--- tests/functional/status.test.js
+++ tests/functional/status.test.js
@@ -5,21 +5,25 @@ const {remoteSwarmHook, localSwarmHooks} = require('../shared/hooks');
 
     (harnessConfigs.testRemoteSwarm ? remoteSwarmHook() : localSwarmHooks());
 
-    it('should be able to get status', async function () {
-        await this.api.status();
+    before('make status request', async function () {
+        this.response = await this.api.status();
+
+        this.moduleStatusJson = JSON.parse(this.response.moduleStatusJson)
     });
 
-    it('response should confirm to schema', async function () {
+    it('status response should conform to status schema', async function () {
+        expect(this.response).to.be.jsonSchema(statusSchema);
+    });
 
-        const res = await this.api.status();
+    it('moduleStatusJson[0] response should conform to status schema', async function () {
+        expect(this.moduleStatusJson.module[0]).to.be.jsonSchema(pbftModuleSchema);
+    });
 
-        expect(res).to.be.jsonSchema(statusSchema);
-
-        const moduleStatusJson = JSON.parse(res.moduleStatusJson).module[0].status;
-
-        expect(moduleStatusJson).to.be.jsonSchema(moduleStatusJsonSchema);
+    it('moduleStatusJson[1] response should conform to crud schema', async  function () {
+        expect(this.moduleStatusJson.module[1]).to.be.jsonSchema(crudModuleSchema);
     });
 });
+
 
 const statusSchema = {
     properties: {
@@ -29,62 +33,129 @@ const statusSchema = {
         swarmGitCommit: {
             type: 'string'
         },
+        swarmId: {
+            type: 'string'
+        },
         uptime: {
             type: 'string'
         },
         moduleStatusJson: {
             type: 'string'
+        },
+        pbftEnabled: {
+            type: 'boolean'
         }
-    }
+    },
+    required: [
+        'swarmVersion',
+        'swarmGitCommit',
+        'swarmId',
+        'uptime',
+        'moduleStatusJson',
+        'pbftEnabled'
+    ],
+    additionalProperties: false
 };
 
-const moduleStatusJsonSchema = {
+const pbftModuleSchema = {
     properties: {
-        is_primary: {
-            type: 'boolean'
+        name: {
+            type: 'string'
         },
-        latest_checkpoint: {
+        status: {
             properties: {
-                hash: {
-                    type: 'string'
+                is_primary: {
+                    type: 'boolean'
                 },
-                sequence_number: {
-                    type: 'number'
+                latest_checkpoint: {
+                    properties: {
+                        hash: {
+                            type: 'string'
+                        },
+                        sequence_number: {
+                            type: 'number'
+                        }
+                    }
+                },
+                latest_stable_checkpoint: {
+                    properties: {
+                        hash: {
+                            type: 'string'
+                        },
+                        sequence_number: {
+                            type: 'number'
+                        }
+                    }
+                },
+                next_issued_sequence_number: {
+                    type: 'number',
+                    minimum: 0
+                },
+                outstanding_operations_count: {
+                    type: 'number',
+                    minimum: 0
+                },
+                peer_index: {
+                    type: 'array',
+                    minimum: 2
+                },
+                primary: {
+                    properties: {
+                        host: {
+                            type: 'string'
+                        },
+                        host_port: {
+                            type: 'number'
+                        },
+                        uuid: {
+                            type: 'string'
+                        }
+                    },
+                    required: [
+                        'host',
+                        'host_port',
+                        'uuid'
+                    ]
+                },
+                view: {
+                    type: 'number',
+                    minimum: 0
                 }
-            }
-        },
-        next_issued_sequence_number: {
-            type: 'number',
-            minimum: 0
-        },
-        outstanding_operations_count: {
-            type: 'number',
-            minimum: 0
-        },
-        peer_index: {
-            type: 'array',
-            minimum: 2
-        },
-        primary: {
-            properties: {
-                host: {
-                    type: 'string'
-                },
-                host_port: {
-                    type: 'number'
-                },
-                uuid: {
-                    type: 'string'
-                }
-            }
-        },
-        unstable_checkpoints_count: {
-            type: 'number',
-            minimum: 0
-        },
-        view: {
-            type: 'number',
-            minimum: 0
+            },
+            required: [
+                'is_primary',
+                'latest_checkpoint',
+                'latest_stable_checkpoint',
+                'next_issued_sequence_number',
+                'outstanding_operations_count',
+                'peer_index',
+                'primary',
+                'view'
+            ],
+            additionalProperties: false
         }
-    }
+    },
+    required: ['name', 'status']
+};
+
+const crudModuleSchema = {
+    properties: {
+        name: {
+            type: 'string'
+        },
+        status: {
+            properties: {
+                max_swarm_storage: {
+                    type: 'integer'
+                },
+                swarm_storage_usage: {
+                    type: 'integer'
+                }
+            },
+            required: ['max_swarm_storage', 'swarm_storage_usage']
+        }
+
+    },
+    required: ['name', 'status'],
+    additionalProperties: false
 };
