4c397a0becd26aa96587f53ee38f48161570ab89
John <john@bluzelle.com>
2019-08-08 16:17:51 -0700

    KEP-1583 Add status crud module tests (#48)
    * Update status request tests, update pbftModule and crudModule schemas
    
    * Add crud module tests
    
    * Clean up tests
    
    * Dynamically generate schema tests, add env variable to set max_swarm_storage

--- tests/functional/status.test.js
+++ tests/functional/status.test.js
@@ -1,23 +1,62 @@
 const {remoteSwarmHook, localSwarmHooks} = require('../shared/hooks');
+const {initializeClient} = require('../../src/clientManager');
+const {find} = require('lodash');
 
 
 (harnessConfigs.testRemoteSwarm ? describe.only : describe)('status', function () {
 
-    (harnessConfigs.testRemoteSwarm ? remoteSwarmHook() : localSwarmHooks());
+    const testParams = {
+        numberOfNamespaces: 3,
+        namespaceSize: 5000,
+        maxSwarmStorage: harnessConfigs.maxSwarmStorage
+    };
 
-    it('should be able to get status', async function () {
-        await this.api.status();
+    const modules = [];
+
+    (harnessConfigs.testRemoteSwarm ? remoteSwarmHook() : localSwarmHooks({configOptions: {max_swarm_storage: testParams.maxSwarmStorage}}));
+
+    before('make status request', async function () {
+        this.response = await this.api.status();
+        JSON.parse(this.response.moduleStatusJson).module.forEach(module => modules.push(module));
     });
 
-    it('response should confirm to schema', async function () {
+    it('status response should conform to status schema', function () {
+        expect(this.response).to.be.jsonSchema(statusSchema);
+    });
 
-        const res = await this.api.status();
+    // dummy test to force tests to be generated async
+    it('tests after status request', function (done) {
 
-        expect(res).to.be.jsonSchema(statusSchema);
+        describe('module tests', function () {
 
-        const moduleStatusJson = JSON.parse(res.moduleStatusJson).module[0].status;
+            modules.forEach(module => {
 
-        expect(moduleStatusJson).to.be.jsonSchema(moduleStatusJsonSchema);
+                it(`${module.name} response should conform to ${module.name} schema`, function () {
+                    expect(module).to.be.jsonSchema(schemas[`${module.name}ModuleSchema`]);
+                });
+            })
+        })
+        done();
+    })
+
+    context('crud module', function () {
+
+        before('create name spaces', async function () {
+            const uuids = [...Array(testParams.numberOfNamespaces)].map(() => `${Math.random()}`);
+            const clients = await Promise.all(uuids.map(uuid => initializeClient({uuid, esrContractAddress: this.swarmManager.getEsrContractAddress()})));
+            await Promise.all(clients.map(apis => apis[0]._createDB(testParams.namespaceSize)));
+
+            const response = await this.api.status();
+            this.crudModuleResponse = find(JSON.parse(response.moduleStatusJson).module, (module) => module.name === 'crud');
+        });
+
+        it('should report correct swarm_storage_usage', function () {
+            expect(this.crudModuleResponse.status).to.deep.include({'swarm_storage_usage': testParams.namespaceSize * testParams.numberOfNamespaces})
+        });
+
+        it('should report correct max_swarm_storage', function () {
+            expect(this.crudModuleResponse.status).to.deep.include({'max_swarm_storage': testParams.maxSwarmStorage})
+        });
     });
 });
 
@@ -29,62 +68,135 @@ const statusSchema = {
         swarmGitCommit: {
             type: 'string'
         },
+        swarmId: {
+            type: 'string'
+        },
         uptime: {
             type: 'string'
         },
         moduleStatusJson: {
             type: 'string'
+        },
+        pbftEnabled: {
+            type: 'boolean'
         }
-    }
+    },
+    required: [
+        'swarmVersion',
+        'swarmGitCommit',
+        'swarmId',
+        'uptime',
+        'moduleStatusJson',
+        'pbftEnabled'
+    ],
+    additionalProperties: false
 };
 
-const moduleStatusJsonSchema = {
+const pbftModuleSchema = {
     properties: {
-        is_primary: {
-            type: 'boolean'
+        name: {
+            type: 'string'
         },
-        latest_checkpoint: {
+        status: {
             properties: {
-                hash: {
-                    type: 'string'
+                is_primary: {
+                    type: 'boolean'
                 },
-                sequence_number: {
-                    type: 'number'
+                latest_checkpoint: {
+                    properties: {
+                        hash: {
+                            type: 'string'
+                        },
+                        sequence_number: {
+                            type: 'number'
+                        }
+                    }
+                },
+                latest_stable_checkpoint: {
+                    properties: {
+                        hash: {
+                            type: 'string'
+                        },
+                        sequence_number: {
+                            type: 'number'
+                        }
+                    }
+                },
+                next_issued_sequence_number: {
+                    type: 'number',
+                    minimum: 0
+                },
+                outstanding_operations_count: {
+                    type: 'number',
+                    minimum: 0
+                },
+                peer_index: {
+                    type: 'array',
+                    minimum: 2
+                },
+                primary: {
+                    properties: {
+                        host: {
+                            type: 'string'
+                        },
+                        host_port: {
+                            type: 'number'
+                        },
+                        uuid: {
+                            type: 'string'
+                        }
+                    },
+                    required: [
+                        'host',
+                        'host_port',
+                        'uuid'
+                    ]
+                },
+                view: {
+                    type: 'number',
+                    minimum: 0
                 }
-            }
-        },
-        next_issued_sequence_number: {
-            type: 'number',
-            minimum: 0
-        },
-        outstanding_operations_count: {
-            type: 'number',
-            minimum: 0
-        },
-        peer_index: {
-            type: 'array',
-            minimum: 2
-        },
-        primary: {
-            properties: {
-                host: {
-                    type: 'string'
-                },
-                host_port: {
-                    type: 'number'
-                },
-                uuid: {
-                    type: 'string'
-                }
-            }
-        },
-        unstable_checkpoints_count: {
-            type: 'number',
-            minimum: 0
-        },
-        view: {
-            type: 'number',
-            minimum: 0
+            },
+            required: [
+                'is_primary',
+                'latest_checkpoint',
+                'latest_stable_checkpoint',
+                'next_issued_sequence_number',
+                'outstanding_operations_count',
+                'peer_index',
+                'primary',
+                'view'
+            ],
+            additionalProperties: false
         }
-    }
+    },
+    required: ['name', 'status']
 };
+
+const crudModuleSchema = {
+    properties: {
+        name: {
+            type: 'string'
+        },
+        status: {
+            properties: {
+                max_swarm_storage: {
+                    type: 'integer'
+                },
+                swarm_storage_usage: {
+                    type: 'integer'
+                }
+            },
+            required: ['max_swarm_storage', 'swarm_storage_usage']
+        }
+
+    },
+    required: ['name', 'status'],
+    additionalProperties: false
+};
+
+const schemas = {
+    statusSchema,
+    pbftModuleSchema,
+    crudModuleSchema
+}
